3401b79fde4949687dd4b2ad690e3d5cfcd72412
tatu-m <144846801+tatumertanen@users.noreply.github.com>
2024-08-19 12:22:54 +0300

    Add HOSTNAME and GTAG consts

--- consts.ts
+++ consts.ts
@@ -1,2 +1,3 @@
 export const DISCORD_INVITE = "https://discord.gg/mKtJDGHRpj";
-export const REPO_URL = "https://github.com/tatumertanen/ultimate-self-improvement-guide";
+export const REPO_URL = "https://github.com/tatumertanen/ultimate-self-improvement-guide";export const HOSTNAME = "https://habitmaxx.com";
+export const GTAG = "G-J20R7BZ3W9";
