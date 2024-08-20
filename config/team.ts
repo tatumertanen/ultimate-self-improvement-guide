562b31f5f87cc1242be50057b6bc8395f147c8ed
tatu-m <144846801+tatumertanen@users.noreply.github.com>
2024-08-20 19:21:41 +0300

    Create team.ts

---
+++ config/team.ts
@@ -0,0 +1,14 @@
+export const members = [
+	{
+		avatar: "https://www.github.com/tatumertanen.png",
+		name: "Tatu",
+		title: "Founder",
+		links: [{ icon: "github", link: "https://github.com/tatumertanen" }],
+	},
+	{
+		avatar: "https://www.github.com/.png",
+		name: "",
+		title: "Co-Founder",
+		links: [{ icon: "github", link: "https://github.com/" }],
+	},
+];
