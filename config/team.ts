efd5ee6013207969832930922b9804cca91eb470
tatu-m <144846801+tatumertanen@users.noreply.github.com>
2024-08-21 01:01:46 +0300

    Update team.ts

--- config/team.ts
+++ config/team.ts
@@ -1,14 +1,26 @@
+import { DISCORD_INVITE } from "../consts";
+
 export const members = [
 	{
 		avatar: "https://www.github.com/tatumertanen.png",
 		name: "Tatu",
 		title: "Founder",
-		links: [{ icon: "github", link: "https://github.com/tatumertanen" }],
+		links: [
+			{ icon: "discord", link: DISCORD_INVITE },
+			{ icon: "github", link: "https://github.com/tatumertanen" },
+			// https://vitepress.dev/reference/default-theme-nav#social-links
+		],
 	},
 	{
-		avatar: "https://www.github.com/.png",
-		name: "",
-		title: "Co-Founder",
-		links: [{ icon: "github", link: "https://github.com/" }],
+		avatar: "/favicon.png",
+		name: "X",
+		title: "Maintainer",
+		links: [{ icon: "discord", link: DISCORD_INVITE }],
+	},
+	{
+		avatar: "/favicon.png",
+		name: "X",
+		title: "Maintainer",
+		links: [{ icon: "discord", link: DISCORD_INVITE }],
 	},
 ];
