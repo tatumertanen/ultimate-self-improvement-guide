0c040d97b8bc28a299dce973d067861b7ac2e7e8
tatu-m <144846801+tatumertanen@users.noreply.github.com>
2024-08-21 16:39:05 +0300

    Update team.ts
    change habitmaxx community title, reformat code

--- config/team.ts
+++ config/team.ts
@@ -1,26 +1,26 @@
 import { DISCORD_INVITE } from "../consts";
 
 export const members = [
-	{
-		avatar: "https://www.github.com/tatumertanen.png",
-		name: "Tatu M",
-		title: "Founder",
-		links: [
-			{ icon: "discord", link: DISCORD_INVITE },
-			{ icon: "github", link: "https://github.com/tatumertanen" },
-			// https://vitepress.dev/reference/default-theme-nav#social-links
-		],
-	},
-	{
-		avatar: "https://avatars.githubusercontent.com/u/179009542",
-		name: "Joel V",
-		title: "Co-Founder",
-		links: [{ icon: "discord", link: DISCORD_INVITE }],
-	},
-	{
-		avatar: "/logo.png",
-		name: "Habitmaxx Community",
-		title: "Maintainer",
-		links: [{ icon: "discord", link: DISCORD_INVITE }],
-	},
+    {
+        avatar: "https://www.github.com/tatumertanen.png",
+        name: "Tatu M",
+        title: "Founder",
+        links: [
+            { icon: "discord", link: DISCORD_INVITE },
+            { icon: "github", link: "https://github.com/tatumertanen" },
+            // https://vitepress.dev/reference/default-theme-nav#social-links
+        ],
+    },
+    {
+        avatar: "https://avatars.githubusercontent.com/u/179009542",
+        name: "Joel V",
+        title: "Co-Founder",
+        links: [{ icon: "discord", link: DISCORD_INVITE }],
+    },
+    {
+        avatar: "/logo.png",
+        name: "Habitmaxx Community",
+        title: "Editor",
+        links: [{ icon: "discord", link: DISCORD_INVITE }],
+    },
 ];
