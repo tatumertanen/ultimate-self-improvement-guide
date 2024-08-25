7dcd9df90cae58b138a0990c171f881a2c0ab8ab
tatu-m <144846801+tatumertanen@users.noreply.github.com>
2024-08-25 13:35:20 +0300

    Update consts.ts
    Add NAME const

--- consts.ts
+++ consts.ts
@@ -1,3 +1,5 @@
+export const NAME = "Habitmaxx";
+
 export const DISCORD_INVITE = "https://discord.gg/mKtJDGHRpj";
 export const REPO_URL = "https://github.com/tatumertanen/ultimate-self-improvement-guide";
 export const HOSTNAME = "https://habitmaxx.com";
